8920ff169a9df8bc2b6d25dd5a137d78720b9667
tyon2006 <nicholas.tyo@gmail.com>
2020-01-23 21:06:07 -0500

    Beta 1
    Beta 1 for Curseforge page

--- src/main/java/com/aetheriumashenarmor/AetheriumAshenArmor.java
+++ src/main/java/com/aetheriumashenarmor/AetheriumAshenArmor.java
@@ -10,7 +10,7 @@ public class AetheriumAshenArmor
 {
     public static final String MODID = "aetheriumashenarmor";
     public static final String NAME = "Aetherium Ashen Armor";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
